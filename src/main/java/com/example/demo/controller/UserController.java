efd5ec39bda98386f61e6bccb2b4d06fc2795217
sem <>
2024-10-14 23:17:39 +0900

    사번이 포함된 도메인네임에서 사번을 추출하여 리턴

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,27 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.transform.sax.SAXResult;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class UserController {
 
-    // 입력 인자 없이 고정된 사번을 반환하는 API
     @GetMapping("/api/v1/user")
-    public Map<String, String> getUserId() {
-        // 사번을 JSON 형식으로 응답
+    public Map<String, String> getUserNo(HttpServletRequest request) {
+
+        // 사번이 포함된 도메인 네임(ex: [사번].ce-aa.kubepia.net)
+        String serverName = request.getServerName();
+
+        // 도메인네임에서 사번만 추출
+        String userNo = serverName.split("\\.")[0];
+
         Map<String, String> response = new HashMap<>();
-        response.put("user_id", "82037161");  // 고정된 사번
+        response.put("userNo", userNo);
         return response;
     }
 }
